5092b28a51a10ea4becb9e796a8fec47e90937eb
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-25 18:28:42 -0500

    Added VersionFile

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     java
     `java-library`
     id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("org.kakara.versionfile") version "1.0.0"
     `maven-publish`
     signing
 }
@@ -19,7 +20,9 @@ var lwjglNatives = when (OperatingSystem.current()) {
         else
             "natives-linux"
     }
-    OperatingSystem.WINDOWS -> if (System.getProperty("os.arch").contains("64")) "natives-windows" else "natives-windows-x86"
+    OperatingSystem.WINDOWS -> if (System.getProperty("os.arch")
+            .contains("64")
+    ) "natives-windows" else "natives-windows-x86"
     OperatingSystem.MAC_OS -> "natives-macos"
     else -> throw Error("Unrecognized or unsupported Operating system. Please set \"lwjglNatives\" manually")
 }
@@ -96,8 +99,15 @@ tasks {
             archiveFileName.set("${project.name}-${project.version}-${project.property("native")}.jar")
         }
     }
+    "jar"{
+        dependsOn(project.tasks.getByName("vftask"));
+    }
 }
 
+versionFileConfig {
+    isCompileIntoJar = true;
+    jarDirectory = "engine"
+}
 dependencies {
     // Regular Depends
     implementation(group = "me.ryandw11", name = "Octree", version = "1.0")
