829712c4bf4e36227d615b8852f1d898856124b1
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-30 10:01:49 -0500

    Fixed javadoc

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ mkdir archives
 gradle clean shadowJar -PbuildNumber=${BUILD_NUMBER} -Pnative=natives-linux -Pis-build=true
 cp build/libs/*.jar archives/
 
-gradle clean shadowJar -PbuildNumber=${BUILD_NUMBER} -Pnative=natives-windows -Pis-build=true
+gradle clean shadowJar javadoc -PbuildNumber=${BUILD_NUMBER} -Pnative=natives-windows -Pis-build=true
 cp build/libs/*.jar archives/
+
 
 ls archives/ > files.txt
