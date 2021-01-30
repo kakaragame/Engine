c656f2750c75c4b1d1bb5622ab09a007eb85038d
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-30 11:01:18 -0500

    Testing

--- buildSrc/src/main/java/org/kakara/engine/Version.java
+++ buildSrc/src/main/java/org/kakara/engine/Version.java
@@ -35,6 +35,12 @@ public class Version {
         return value;
     }
 
+    /**
+     * Executes a command
+     * @param cmd the command
+     * @return the return
+     * @throws IOException if it failed.
+     */
     public static String execCmd(String cmd) throws IOException {
         java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
